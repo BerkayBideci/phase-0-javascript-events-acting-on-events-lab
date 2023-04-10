14a1af9532f16ef4fc84676a57b729d17fe6a4a6
Berkay Bideci <berkaybideci@gmail.com>
2023-04-10 17:42:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "200px"
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`
+    }
+}
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 10}px`
+    }
+}
+
+function moveDodgerUp() {
+    const upNumbers = dodger.style.bottom.replace("px", "");
+    const up = parseInt(upNumbers, 10);
+
+    if (up < 380) {
+        dodger.style.bottom = `${up + 10}px`
+    }
+}
+
+function moveDodgerDown() {
+    const downNumbers = dodger.style.bottom.replace("px", "");
+    const down = parseInt(downNumbers, 10);
+
+    if (down > 0) {
+        dodger.style.bottom = `${down - 10}px`
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    } else if (e.key === "ArrowUp") {
+        moveDodgerUp();
+    } else if (e.key === "ArrowDown") {
+        moveDodgerDown();
+    }
+})
